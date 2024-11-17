003e19519779ed2a3901dfc26b334a23d2d51847
Florin0216 <florin.barbu16@yahoo.com>
2024-11-17 16:06:42 +0200

    Final commit

--- src/Cinema/Main.java
+++ src/Cinema/Main.java
@@ -2,7 +2,7 @@ package Cinema;
 
 import java.util.Scanner;
 
-public class Cinema {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
